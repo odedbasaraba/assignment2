39d8c4cb752b466c52a4ece633101830237ecbcf
oded basaraba <odedbasaraba08@gmail.com>
2023-10-14 11:30:58 +0300

    finished cnn

--- cs231n/classifiers/cnn.py
+++ cs231n/classifiers/cnn.py
@@ -1,9 +1,9 @@
 from builtins import object
 import numpy as np
 
-from ..layers import *
-from ..fast_layers import *
-from ..layer_utils import *
+from cs231n.layers import *
+from cs231n.fast_layers import *
+from cs231n.layer_utils import *
 
 
 class ThreeLayerConvNet(object):
@@ -17,17 +17,9 @@ class ThreeLayerConvNet(object):
     channels.
     """
 
-    def __init__(
-        self,
-        input_dim=(3, 32, 32),
-        num_filters=32,
-        filter_size=7,
-        hidden_dim=100,
-        num_classes=10,
-        weight_scale=1e-3,
-        reg=0.0,
-        dtype=np.float32,
-    ):
+    def __init__(self, input_dim=(3, 32, 32), num_filters=32, filter_size=7,
+                 hidden_dim=100, num_classes=10, weight_scale=1e-3, reg=0.0,
+                 dtype=np.float32):
         """
         Initialize a new network.
 
@@ -59,13 +51,25 @@ class ThreeLayerConvNet(object):
         # IMPORTANT: For this assignment, you can assume that the padding          #
         # and stride of the first convolutional layer are chosen so that           #
         # **the width and height of the input are preserved**. Take a look at      #
-        # the start of the loss() function to see how that happens.                #
+        # the start of the loss() function to see how that happens.                #                           
         ############################################################################
-        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        
+        C, H, W = input_dim
+        
+        # Convolutional layer
+        self.params['W1'] = np.random.randn(num_filters, C, filter_size, filter_size) * weight_scale
+        self.params['b1'] = np.zeros(num_filters)
 
-        pass
+        # Hidden affine layer
+        # Note that the width and height are preserved after the convolutional layer
+        # 2x2 max pool makes the width and height reduce by half
+        self.params['W2'] = np.random.randn(num_filters*(H//2)*(W//2), hidden_dim) * weight_scale
+        self.params['b2'] = np.zeros(hidden_dim)
 
-        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        # Output affine layer
+        self.params['W3'] = np.random.randn(hidden_dim, num_classes) * weight_scale
+        self.params['b3'] = np.zeros(num_classes)
+
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -73,23 +77,24 @@ class ThreeLayerConvNet(object):
         for k, v in self.params.items():
             self.params[k] = v.astype(dtype)
 
+
     def loss(self, X, y=None):
         """
         Evaluate loss and gradient for the three-layer convolutional network.
 
         Input / output: Same API as TwoLayerNet in fc_net.py.
         """
-        W1, b1 = self.params["W1"], self.params["b1"]
-        W2, b2 = self.params["W2"], self.params["b2"]
-        W3, b3 = self.params["W3"], self.params["b3"]
+        W1, b1 = self.params['W1'], self.params['b1']
+        W2, b2 = self.params['W2'], self.params['b2']
+        W3, b3 = self.params['W3'], self.params['b3']
 
         # pass conv_param to the forward pass for the convolutional layer
         # Padding and stride chosen to preserve the input spatial size
         filter_size = W1.shape[2]
-        conv_param = {"stride": 1, "pad": (filter_size - 1) // 2}
+        conv_param = {'stride': 1, 'pad': (filter_size - 1) // 2}
 
         # pass pool_param to the forward pass for the max-pooling layer
-        pool_param = {"pool_height": 2, "pool_width": 2, "stride": 2}
+        pool_param = {'pool_height': 2, 'pool_width': 2, 'stride': 2}
 
         scores = None
         ############################################################################
@@ -100,11 +105,13 @@ class ThreeLayerConvNet(object):
         # Remember you can use the functions defined in cs231n/fast_layers.py and  #
         # cs231n/layer_utils.py in your implementation (already imported).         #
         ############################################################################
-        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        
+        conv_relu_pool_out, conv_relu_pool_cache = conv_relu_pool_forward(X, W1, b1,
+            conv_param, pool_param)
+        affine_relu_out, affine_relu_cache = affine_relu_forward(conv_relu_pool_out,
+            W2, b2)
+        scores, scores_cache = affine_forward(affine_relu_out, W3, b3)
 
-        pass
-
-        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -123,11 +130,21 @@ class ThreeLayerConvNet(object):
         # automated tests, make sure that your L2 regularization includes a factor #
         # of 0.5 to simplify the expression for the gradient.                      #
         ############################################################################
-        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        
+        loss, dscores = softmax_loss(scores, y)
+        loss += 0.5 * self.reg * (np.sum(W1*W1) + np.sum(W2*W2) + np.sum(W3*W3))
 
-        pass
+        daffine_relu_out, dW3, db3 = affine_backward(dscores, scores_cache)
+        dconv_relu_pool_out, dW2, db2 = affine_relu_backward(daffine_relu_out, affine_relu_cache)
+        dx, dW1, db1 = conv_relu_pool_backward(dconv_relu_pool_out, conv_relu_pool_cache)
 
-        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        grads['W1'] = dW1 + self.reg * W1
+        grads['W2'] = dW2 + self.reg * W2
+        grads['W3'] = dW3 + self.reg * W3
+        grads['b1'] = db1
+        grads['b2'] = db2
+        grads['b3'] = db3
+
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
